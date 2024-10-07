0d08aa25d6b73bfb44c06e644633d1b3164b1621
IgorLap <igor.laptev@myget-it.com>
2024-10-07 17:02:33 +0300

    feat: cli task solved

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const props = {};
+  
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].replace(/^--/, '');
+      const propValue = args[i + 1];
+      props[propName] = propValue;
+    }
+  
+    for (const propName in props) {
+      console.log(`${propName} is ${props[propName]}`);
+    }
 };
 
 parseArgs();
