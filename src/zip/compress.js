8b62b7f625f5352cec09339b807ae3ebc91e5bf3
IgorLap <igor.laptev@myget-it.com>
2024-10-07 17:02:57 +0300

    feat: zip task solved

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const fileReadStream = createReadStream("files/fileToCompress.txt");
+    const gzipStream = createGzip();
+    const archiveWriteStream = createWriteStream("files/archive.gz");
+
+    fileReadStream.pipe(gzipStream).pipe(archiveWriteStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const archiveReadStream = createReadStream("files/archive.gz");
+    const gunzipStream = createGunzip();
+    const fileWriteStream = createWriteStream("files/fileToCompress.txt");
+
+    archiveReadStream.pipe(gunzipStream).pipe(fileWriteStream);
 };
 
 await decompress();
