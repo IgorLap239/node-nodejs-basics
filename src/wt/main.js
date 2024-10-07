89f1b89670359dffbb44b1d6b55f8742299876b3
IgorLap <igor.laptev@myget-it.com>
2024-10-07 17:03:58 +0300

    feat: wt task solved

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = os.cpus().length;
+    const workers = [];
+    const results = [];
+  
+    for (let i = 0; i < numCores; i++) {
+      const worker = new Worker('./worker.js');
+      workers.push(worker);
+      worker.postMessage(i + 10);
+      worker.on('message', (event) => {
+        results.push(event);
+        if (results.length === numCores) {
+          console.log(results);
+        }
+      });
+    }
 };
 
 await performCalculations();
