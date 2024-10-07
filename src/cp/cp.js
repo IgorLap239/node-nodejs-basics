df82df45c53aa89f640d41b2f092400ac0df33c6
IgorLap <igor.laptev@myget-it.com>
2024-10-07 17:03:44 +0300

    feat: cp task solved

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', ['./files/script.js', ...args], {
+        stdio: ['inherit', 'pipe', 'inherit', 'ipc']
+    });
+
+    child.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    process.stdin.pipe(child);
+
+    return;
 };
 
 // Put your arguments in function call to test this functionality
